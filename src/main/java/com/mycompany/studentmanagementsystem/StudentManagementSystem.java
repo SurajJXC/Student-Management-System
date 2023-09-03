c9967226f8a3fc2f4ee5d4665c96d973d2ad0417
SurajJXC <142650979+SurajJXC@users.noreply.github.com>
2023-09-04 03:30:14 +0530

    Initial commit

---
+++ src/main/java/com/mycompany/studentmanagementsystem/StudentManagementSystem.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.studentmanagementsystem;
+
+import com.mysql.cj.protocol.Resultset;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author dhaka
+ */
+public class StudentManagementSystem {   
+    
+    public static void main(String[] args) throws SQLException {
+        
+        System.out.println("Hello World!");
+        UIcomponent frame = new UIcomponent();        
+
+    // Set the JFrame visible
+    frame.setVisible(true);
+    }}
+    
+   
